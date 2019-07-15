5d2f5ccfc4fc99b7eea41ee09d2b52d9e7289c01
Marcus <marcuskastner@web.de>
2019-07-15 20:07:08 +0200

    updates socket permission

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,6 +9,10 @@ dockerize -template /srv/templates/:/etc/rspamd/local.d/
 
 # add whitelist
 echo $DOMAIN_WHITELIST | tr -s ',' '\n' > /etc/rspamd/local.d/whitelist.sender.domain.map
+
+#permissions
+mkdir -p /run/rspamd
+chown -R rspamd:rspamd /run/rspamd
 chown -R rspamd:rspamd /etc/rspamd/
 
 # start cron
