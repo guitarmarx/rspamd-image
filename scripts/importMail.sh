314baa238fe06fd3cf434da062357df65cc3822e
Marcus Kastner <marcus.kastner@meteorit-leipzig.de>
2022-05-24 15:52:17 +0200

    added rspamc, new spam import

--- scripts/importMail.sh
+++ scripts/importMail.sh
@@ -1,11 +1,27 @@
-#!/bin/bash
+#!/bin/sh
+
+# import spam
 if [ "$(ls -A ${SPAM_IMPORT_FOLDER}/)" ]; then
+    mkdir -p ${SPAM_IMPORT_FOLDER}/learned
     for file in ${SPAM_IMPORT_FOLDER}/*.eml; do
-        rspamc learn_spam -h 127.0.0.1:80  $file
+        rspamc learn_spam -h 127.0.0.1:11334  $file
         status=$?
 
         if [ $status -eq 0 ]; then
-            rm $file
+            mv $file ${SPAM_IMPORT_FOLDER}/learned
+        fi
+    done
+fi
+
+# import ham
+if [ "$(ls -A ${HAM_IMPORT_FOLDER}/)" ]; then
+    mkdir -p ${HAM_IMPORT_FOLDER}/learned
+    for file in ${HAM_IMPORT_FOLDER}/*.eml; do
+        rspamc learn_ham -h 127.0.0.1:11334  $file
+        status=$?
+
+        if [ $status -eq 0 ]; then
+            mv $file ${HAM_IMPORT_FOLDER}/learned
         fi
     done
 fi
